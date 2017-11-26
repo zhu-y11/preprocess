47ff6f73ba3662b75ff3ad60b9b8815513321285
Yi Zhu <zhu-y11@foxmail.com>
2017-11-26 19:15:11 +0000

    initial commit

---
+++ conv_input.py
@@ -0,0 +1,61 @@
+# -*- coding: UTF-8 -*-
+#!/usr/bin/python3
+"""
+Convert input format of Wiki data for different model
+@Author Yi Zhu
+Upated 25/11/2017
+"""
+
+#************************************************************
+# Imported Libraries
+#************************************************************
+import argparse
+from tqdm import tqdm
+import os
+
+
+def main(args):
+  conv2SentSplit(args)
+  conv2DocSplit(args)
+  
+
+def conv2SentSplit(args):
+  with open(args.output_file + '.sent', 'w') as f1:
+    for root, dirs, files in tqdm(os.walk(args.input_dir), total = len(os.listdir(args.input_dir))):
+      for file_name in files:
+        # input file
+        in_file = os.path.join(root, file_name)
+
+        with open(in_file, 'r') as f:
+          docs = f.read().strip().split('\n' * 4)
+          docs = [doc.strip().split('\n\n') for doc in docs]
+          docs = ['\n'.join(doc) for doc in docs]
+          f1.write('\n'.join(docs) + '\n')
+
+
+def conv2DocSplit(args):
+  with open(args.output_file + '.doc', 'w') as f1:
+    for root, dirs, files in tqdm(os.walk(args.input_dir), total = len(os.listdir(args.input_dir))):
+      for file_name in files:
+        # input file
+        in_file = os.path.join(root, file_name)
+
+        with open(in_file, 'r') as f:
+          docs = f.read().strip().split('\n' * 4)
+          for i, doc in enumerate(docs[:]):
+            sents = doc.strip().split('\n')
+            sents = [sent.strip() for sent in sents if sent.strip()]
+            docs[i] = ' '.join(sents)
+          f1.write('\n'.join(docs) + '\n')
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser('Input Conversion')
+  parser.add_argument('-m', '--model', default = 'word2vec',
+                      help = 'word embedding model')
+  parser.add_argument('-i', '--input_dir', required = True,
+                      help = 'input wiki data dir')
+  parser.add_argument('-o', '--output_file', default = 'corpus',
+                      help = 'output wiki data file')
+  args = parser.parse_args()
+  main(args)
---
+++ wiki_preproc.py
@@ -0,0 +1,88 @@
+# -*- coding: UTF-8 -*-
+#!/usr/bin/python3
+"""
+Preprocessing wiki text
+@Author Yi Zhu
+Upated  26/11/2017
+"""
+
+#************************************************************
+# Imported Libraries
+#************************************************************
+import os
+import re
+import argparse
+from tqdm import tqdm
+import string
+
+import polyglot
+from polyglot.text import Text, Word
+from polyglot.tokenize import SentenceTokenizer
+from polyglot.base import Sequence
+
+
+def main(args):
+  for root, dirs, files in tqdm(os.walk(args.input_dir), total = len(os.listdir(args.input_dir))):
+    for file_name in tqdm(files, total = len(os.listdir(root))):
+      # input file
+      in_file = os.path.join(root, file_name)
+      # create the same dir in output_dir
+      rel_in_path = os.path.dirname(os.path.relpath(in_file, args.input_dir))
+      if not os.path.exists(os.path.join(args.output_dir, rel_in_path)):
+        os.makedirs(os.path.join(args.output_dir, rel_in_path))
+
+      with open(in_file, 'r') as f, open(os.path.join(args.output_dir, rel_in_path, file_name), 'w') as f1:
+        docs = f.read().strip().split('</doc>')
+        # delete documents with only title
+        docs = [doc.strip() for doc in docs if doc.strip().count('\n') > 1]
+        # discard the head
+        docs = [doc[doc.find('\n') + 1:] for doc in docs]
+        for doc in docs:
+          pars = doc.strip().split('\n')
+          # discard the title
+          pars = [par.strip() for par in pars if par.strip()][1:]
+          new_pars = []
+          for par in pars:
+            par_seq = Sequence(par)
+            st = SentenceTokenizer(locale = args.lang)
+            sents = [sent for sent in st.transform(par_seq)]
+            new_sents = []
+            for sent in sents:
+              try:
+                sent = Text(sent, hint_language_code = args.lang)
+                words = sent.words
+              except:
+                continue
+              words = [word for word in words if word not in string.punctuation]
+              # delete sentence with only one word
+              if len(words) <= 1:
+                continue
+              new_sent = ' '.join(words)
+              new_sent = repMultiDigits(new_sent)
+              new_sent = new_sent.lower()
+              new_sents.append(new_sent)
+            if not new_sents:
+              continue
+            new_pars.append('\n'.join(new_sents))
+          if not new_sents:
+            continue
+          new_doc = '\n\n'.join(new_pars)
+          f1.write(new_doc + '\n' * 5)
+
+
+def repMultiDigits(tok_text):
+  regex_list = [ r'[+-]?(\d+[\,\.]\d+)([eE][+-]?\d+)?', r'\d+']
+  for reg in regex_list:
+    tok_text = re.sub(reg, '#', tok_text)
+  return tok_text
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description = 'Pre-processing for wiki text after extraction')
+  parser.add_argument('input_dir', help = 'input dir')
+  parser.add_argument('-o', '--output_dir', default = 'text',
+                      help = 'output dir')
+  parser.add_argument('--lang', default = 'it',
+                      help = 'language for preprocessing')
+  args = parser.parse_args()
+  main(args)
